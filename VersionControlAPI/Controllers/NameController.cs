a52aa8daff845c09027b97ffeedd9b0ebfe74d5d
cortesne <cortesne@mail.uc.edu>
2026-03-25 21:21:55 -0400

    Add NameController returning my name in JSON

---
+++ VersionControlAPI/Controllers/NameController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VersionControlAPI.Controllers
+{
+
+        [ApiController]
+        [Route("api/[controller]")]
+        public class NameController :ControllerBase
+        {
+            [HttpGet]
+            public IActionResult Get()
+            {
+                return Ok(new
+                {
+                    name = "Nicole Cortes Wright"
+                });
+            }
+
+        }
+
+
+    
+}
